c5d9d8456806550cf1a34244ec0c9973abf13834
uplusplus <yjcpui@gmail.com>
2026-06-10 11:39:51 +0800

    r.sh skip Tests

--- r.sh
+++ r.sh
@@ -4,7 +4,7 @@
 
 sudo apt install openjdk-21-jdk
 export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
-mvn clean install -s mateclaw-server/settings.xml
+mvn clean install -s mateclaw-server/settings.xml -DskipTests
 cd mateclaw-server
 mvn spring-boot:run -s settings.xml  &
 npm install pnpm
